1ff27dde7c26eab1a66b6aa52c7e3546f40d1bb8
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2022-12-19 18:43:29 -0300

    implements jpa entiies user

--- hr-user/src/main/java/com/softWalter/hr/user/doumain/entities/Role.java
+++ hr-user/src/main/java/com/softWalter/hr/user/doumain/entities/Role.java
@@ -1,4 +1,53 @@
 package com.softWalter.hr.user.doumain.entities;
 
-public class Role {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String roleName;
+
+    public Role(long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+
+    public Role() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
--- hr-user/src/main/java/com/softWalter/hr/user/doumain/entities/User.java
+++ hr-user/src/main/java/com/softWalter/hr/user/doumain/entities/User.java
@@ -1,13 +1,27 @@
 package com.softWalter.hr.user.doumain.entities;
 
-import java.io.Serializable;
+import javax.persistence.*;
 
-public class User  implements Serializable {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User(Long id, String username, String email, String password) {
         this.id = id;
@@ -55,5 +69,9 @@ public class User  implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 }
