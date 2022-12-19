8411bfb2791074e22227dba31d8c93500fcc5b38
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2022-12-19 16:04:50 -0300

    implements variable user complet

--- hr-user/src/main/java/com/softWalter/hr/user/doumain/entities/User.java
+++ hr-user/src/main/java/com/softWalter/hr/user/doumain/entities/User.java
@@ -1,6 +1,59 @@
 package com.softWalter.hr.user.doumain.entities;
 
-public class User {
-    private Long id;
+import java.io.Serializable;
 
+public class User  implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
